8212b972d7a16688575beebf4ac5e1184b110f72
Karani <danielevans.karani@gmail.com>
2020-08-26 18:14:55 +0300

    Adds more movie categories

--- app/views.py
+++ app/views.py
@@ -12,10 +12,12 @@ def index():
 
     #Getting popular movie
     popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     print(popular_movies)
     title = 'Home - Welcome to the best Movie Review Website Online'
     message = "Hello world"
-    return render_template('index.html', message = message, title=title,popular = popular_movies )
+    return render_template('index.html', message = message, title=title,popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie  )
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
