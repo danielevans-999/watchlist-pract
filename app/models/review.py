03e2dd24b7c6d9905545227f0920ae84dbab1a83
Karani <danielevans.karani@gmail.com>
2020-08-27 12:46:26 +0300

    Creates a review class

---
+++ app/models/review.py
@@ -0,0 +1,16 @@
+class Review:
+
+    all_reviews = []
+
+    def __init(self, movie_id, title, imageurl, review):
+        self.movie_id = movie_id
+        self.title = title
+        self.imageurl = imageurl
+        self.review = review
+        
+    def save_review(self):
+        Review.all_reviews.append(self)
+
+    @classmethod
+    def clear_reviews(cls):
+        cls.all_reviews.clear()
