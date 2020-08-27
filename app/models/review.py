215837f72744e9bf9dbeef260d3f24370f2df865
Karani <danielevans.karani@gmail.com>
2020-08-27 16:32:37 +0300

    Adds get_review method that retrives reviews for a given movie

--- app/models/review.py
+++ app/models/review.py
@@ -14,3 +14,12 @@ class Review:
     @classmethod
     def clear_reviews(cls):
         cls.all_reviews.clear()
+
+    @classmethod
+    def get_reviews(cls,id):
+        response = []
+
+        for review in cls.all_reviews:
+            if review.movie_id == id:
+                response.append(review)
+        return response
