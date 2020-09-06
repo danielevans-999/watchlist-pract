d81063ac6e4aace83e36044f7a9ccd513bf9aabf
Karani <danielevans.karani@gmail.com>
2020-09-06 11:25:08 +0300

    Adds username column in the users table

--- app/models.py
+++ app/models.py
@@ -45,8 +45,9 @@ class Review:
 
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.column(db.String(255))
+    username = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
