4cb4aa8e44635e827ff426d7db02c4492d663c53
Karani <danielevans.karani@gmail.com>
2020-09-07 11:14:11 +0300

    Adds user authentication

--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -43,11 +49,12 @@ class Review:
         return response
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique = True, index = True)
     pass_secure = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
